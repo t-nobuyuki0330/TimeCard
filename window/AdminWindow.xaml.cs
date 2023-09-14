5de4efb267287a87ecf09475f53057c00b3c984c
t-nobuyuki0330 <t.nobuyuki2002@gmail.com>
2023-09-14 23:21:55 +0900

    ページ遷移の作成

--- window/AdminWindow.xaml.cs
+++ window/AdminWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using TimeCard.info;
+using TimeCard.window.page;
+// using System.Windows.Navigation;
 
 namespace TimeCard.window
 {
@@ -20,9 +22,38 @@ namespace TimeCard.window
     /// </summary>
     public partial class AdminWindow : Window
     {
+        private const string PageDirUri = "/window/page";
+        private Uri AdminPageUri = new Uri( PageDirUri + "/AdminPage.xaml", UriKind.Relative );
+        private Uri UsersPageUri = new Uri( PageDirUri + "/UsersPage.xaml", UriKind.Relative );
+        private Uri StampPageUri = new Uri( PageDirUri + "/StampManagePage.xaml", UriKind.Relative );
+
         public AdminWindow( UserInfo user_info )
         {
             InitializeComponent();
+            AdminWindowFrame.Navigate( AdminPageUri );
+        }
+
+        private void LoginWindowButton_Click( object sender, RoutedEventArgs e )
+        {
+            var login_window = new LoginWindow();
+            login_window.Show();
+
+            this.Close();
+        }
+
+        private void AdminManageButton_Click( object sender, RoutedEventArgs e )
+        {
+            AdminWindowFrame.Navigate( AdminPageUri );
+        }
+
+        private void UserManageButton_Click(object sender, RoutedEventArgs e)
+        {
+            AdminWindowFrame.Navigate( UsersPageUri );
+        }
+
+        private void StampManageButton_Click(object sender, RoutedEventArgs e)
+        {
+            AdminWindowFrame.Navigate( StampPageUri );
         }
     }
 }
